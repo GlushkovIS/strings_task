b8a70effbaf3410a379c7b64d19e8d3c6cb1fb0b
IS Glushkov <53116914+GlushkovIS@users.noreply.github.com>
2020-02-24 21:27:38 +0300

    init

---
+++ string.php
@@ -0,0 +1,59 @@
+<?php
+
+echo '<pre>';
+
+$strArr = [
+    'One',
+    'Two',
+    'Twenty',
+    'Аааааа - ru',
+    'Afffff',
+    'Aaaaaa - en',
+];
+
+/**
+ * Задание №1
+ */
+
+usort($strArr, 'strcmp');
+print_r($strArr);
+
+/**
+ * Задание №2
+ */
+
+$str = 'AAbBbbCcCcAAa';
+
+$strArr = str_split($str);
+
+$previousSymbol = false;
+foreach ($strArr as $key => $symbol) {
+    if ($previousSymbol === $symbol) {
+        unset($strArr[$key]);
+        continue;
+    }
+    $previousSymbol = $symbol;
+}
+echo implode($strArr);
+
+/**
+ * Задание №3
+ */
+
+$string = 'AAbBbbCcCcAAa';
+
+$stringArr = str_split($string);
+
+$previousSym = false;
+foreach ($stringArr as $key => $sym) {
+    if (strcasecmp($previousSym, $sym) === 0) {
+        unset($stringArr[$key]);
+        continue;
+    }
+    $previousSym = $sym;
+}
+
+echo '<br>' . implode($stringArr);
+
+
+
